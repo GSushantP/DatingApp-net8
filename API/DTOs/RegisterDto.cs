5b727749500921391339d28ec719df52c7a716b5
GSushantP <patilsushant07.sp@gmail.com>
2024-08-26 17:54:30 +0530

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -5,8 +5,11 @@ namespace API.DTOs;
 public class RegisterDto
 {
     [Required]
-    public required string Username { get; set; }
-    
+    public string Username { get; set; } = string.Empty;
+    // here we are removing required from this line because it will not work good when comes to validation of our fields.
+    //will remove required and will initialize these values to string.empty.
+
     [Required]
-    public required string Password { get; set; }
+    [StringLength(8, MinimumLength =4)]
+    public string Password { get; set; } = string.Empty;
 }
